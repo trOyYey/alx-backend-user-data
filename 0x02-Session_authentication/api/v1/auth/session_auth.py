b2ce0c806a8b448c52473cb1c7a9085e270f89e7
trOyYey <troyzuwara@gmail.com>
2024-11-15 01:14:44 +0000

    create cclass

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        '''
+        session creation
+        '''
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
