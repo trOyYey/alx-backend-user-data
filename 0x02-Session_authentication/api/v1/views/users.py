bcb372df1a7b28485f62d6987230de76951f78df
trOyYey <troyzuwara@gmail.com>
2024-11-15 00:38:15 +0000

    task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,7 +27,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    user = User.get(user_id)
+    if user_id == 'me':
+        user = request.current_user
+    else:
+        user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
