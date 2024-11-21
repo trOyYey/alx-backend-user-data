097c7811a0b573a072b3f64df7eccac8ebdcd864
trOyYey <troyzuwara@gmail.com>
2024-11-21 11:13:49 +0000

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,9 +9,12 @@ Base = declarative_base()
 
 
 class User(Base):
+    '''
+    user object
+    '''
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
