7bf1891218c48e229b09a4f07bcd323c28e89980
trOyYey <troyzuwara@gmail.com>
2024-11-21 12:08:15 +0000

    UUID and validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 '''
 authentication module
 '''
+import uuid
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
@@ -34,9 +35,25 @@ class Auth:
         user = self._db.add_user(email=email, hashed_password=hashed_password)
         return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        logging validation method
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
+
 
 def _hash_password(password: str) -> bytes:
     """
     Hashing password method
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    """
+    UUID generating method
+    """
+    return str(uuid.uuid4())
