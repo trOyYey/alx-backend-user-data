b3a13d7b6c2fd80d05f774f6bd86076cd6be91f6
trOyYey <troyzuwara@gmail.com>
2024-11-21 21:50:37 +0000

    logiin and logingout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 '''
 basic flask app
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -12,13 +12,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    ''' test route'''
+    '''
+    test route
+    '''
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route("/users", methods=["POST"])
 def users():
-    ''' register route'''
+    '''
+    register route
+    '''
     email = request.form.get("email")
     password = request.form.get("password")
     try:
@@ -26,6 +30,34 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    '''
+    loging route
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user = AUTH.valid_login(email, password)
+    if not user:
+        abort(401)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", AUTH.create_session(email))
+    return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    '''
+    logout route
+    '''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 '''
 authentication module
 '''
+from typing import Union
 import uuid
 import bcrypt
 from db import DB
@@ -57,12 +58,33 @@ class Auth:
         self._db._session.commit()
         return user.session_id
 
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        '''
+        user getter from session id
+        '''
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        '''
+        session destroyer method
+        '''
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return
+
 
 def _hash_password(password: str) -> bytes:
     """
     Hashing password method
     """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid() -> str:
