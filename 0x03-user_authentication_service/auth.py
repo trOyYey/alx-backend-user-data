a9436bdfc9a76826c6a8e9c4055f8653799bf910
trOyYey <troyzuwara@gmail.com>
2024-11-21 21:54:20 +0000

    resset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,6 +79,18 @@ class Auth:
         except Exception:
             return
 
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        passwword resset token 
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
 
 def _hash_password(password: str) -> bytes:
     """
