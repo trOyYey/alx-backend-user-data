5996f3a82d2313915b4bd890cbb1d83f76f87824
trOyYey <troyzuwara@gmail.com>
2024-11-21 22:06:50 +0000

    PUT method for resset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,33 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    '''
+    reset password route
+    '''
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    '''
+    reset password PUT route
+    '''
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,7 +81,7 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """
-        passwword resset token 
+        passwword resset token
         """
         try:
             user = self._db.find_user_by(email=email)
@@ -90,6 +90,19 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''
+        updating password method
+        '''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)
 
 
 def _hash_password(password: str) -> bytes:
